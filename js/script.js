e5a9c117a54e1b51a62266bc56f64f6f614b3743
yurish4e <yurish4e@gmail.com>
2018-11-16 21:40:13 +0200

    ver 0.8

---
+++ js/script.js
@@ -0,0 +1,20 @@
+ $(document).ready(function () {
+     $('.chance_slide').slick({ 
+         autoplay: true,
+         dots: true,
+         arrows: false,
+         fade: true,
+         autoplaySpeed: 1500
+                              });
+     $('.unit_left').slick({ 
+         autoplay: true,
+         dots: true,
+         arrows: true,
+         fade: true,
+         autoplaySpeed: 1500
+                              });
+     $('.personal_info').click(function () {
+         $(".personal_full").toggleClass("personal_show", 250, "easeOutSine");
+     });
+ });
+
