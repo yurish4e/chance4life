aad722a66f87b5417d3e2973100531fb92541a2a
yurish4e <yurish4e@gmail.com>
2018-11-20 09:33:28 +0200

    ver 1.01

--- js/script.js
+++ js/script.js
@@ -16,8 +16,51 @@
      $('.personal_info').click(function () {
          $(".personal_full").toggleClass("personal_show", 250, "easeOutSine");
      });
-     $('.menu_open').click(function () {
+     /*$('.menu_open').click(function () {
          $(".menu").toggleClass("menu_visible");
+     });*/
+     /*закрытие/открытие выбора языка*/
+     $('.nav_lang').click(function () {
+         $(".nav_close").addClass("nav_close_lang");
+         $(".nav_lang_menu").toggleClass("nav_lang_menu_visible");
+          $(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $("#menu_visible"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0 && $(".nav_lang")) { // и не по его дочерним элементам
+			$(".nav_lang_menu").removeClass("nav_lang_menu_visible"); // скрываем его
+            $(".nav_close").removeClass("nav_close_lang");
+         $(".nav_close").removeClass("nav_close_menu");
+		}
+	});
      });
+     /*закрытие/открытие меню*/
+     $('.menu_open').click(function () {
+         $(".nav_close").addClass("nav_close_menu");
+         $(".menu").toggleClass("menu_visible");
+          $(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $("#menu"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+			$(".menu").removeClass("menu_visible"); // скрываем его
+            $(".nav_close").removeClass("nav_close_menu");
+         $(".nav_close").removeClass("nav_close_menu");
+		}
+	});
+     });
+       if ($(window).width() < 1280) {
+         $(".news").toggleClass("container");
+         $(".news_main_title").toggleClass("container");
+         $(".news_wrapper").toggleClass("owl-carousel");
+     }
+     
+     $('.owl-carousel').slick({
+        dots: false,
+      infinite: true,
+      speed: 200,
+      slidesToShow: 1,
+      centerMode: false,
+      variableWidth: true,
+         arrows: false
+      });
  });
 
